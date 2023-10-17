d8abb8a8aef501f5c1b01ac1e41c3a411f088348
e1b21007 <e1b21007@oit.ac.jp>
2023-10-17 15:59:21 +0900

    step5まで

--- src/main/java/oit/is/team3/lec4/controller/Sample41Controller.java
+++ src/main/java/oit/is/team3/lec4/controller/Sample41Controller.java
@@ -31,6 +31,46 @@ public class Sample41Controller {
     return "sample41.html";
   }
 
+  @GetMapping("step3")
+  public String sample43() {
+    return "sample43.html";
+  }
+
+  @GetMapping("step5")
+  public String sample45() {
+    return "sample45.html";
+  }
+
+  @PostMapping("step5")
+  public String sample45(@RequestParam String chamberName, ModelMap model) {
+    ArrayList<Chamber> chambers5 = chamberMapper.selectAllByChamberName(chamberName);
+    model.addAttribute("chambers5", chambers5);
+    return "sample45.html";
+  }
+
+  /**
+   *
+   * @param model Thymeleafにわたすデータを保持するオブジェクト
+   * @param prin  ログインユーザ情報が保持されるオブジェクト
+   * @return
+   *
+   *         Transactionalはメソッドでトランザクション処理を実施したい場合に付与する
+   *         このメソッドが開始するとトランザクションが開始され，メソッドが正常に終了するとDBへのアクセスが確定する（Runtime
+   *         errorなどで止まった場合はロールバックが行われる）
+   */
+  @PostMapping("step3")
+  @Transactional
+  public String sample43(@RequestParam String chamberName, ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    Chamber chamber3 = new Chamber();
+    chamber3.setChamberName(chamberName);
+    chamber3.setUserName(loginUser);
+    chamberMapper.insertChamber(chamber3);
+    model.addAttribute("chamber3", chamber3);
+    // System.out.println("ID:" + chamber3.getId());
+    return "sample43.html";
+  }
+
   /**
    * 指定したidをPATHパラメータで受け取り，そのidに対応するデータを取得して返す
    *
