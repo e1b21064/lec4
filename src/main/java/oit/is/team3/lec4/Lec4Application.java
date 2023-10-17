e9f03e3c1b2a0ef02ddeee31fbaeb27ea0a0a577
e1b21064 <e1b21064@oit.ac.jp>
2023-10-17 14:13:26 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team3/lec4/Lec4Application.java
@@ -0,0 +1,13 @@
+package oit.is.team3.lec4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec4Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team3/lec4/Lec4ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team3.lec4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
